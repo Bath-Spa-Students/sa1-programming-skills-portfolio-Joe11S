68fce5876e23b4500126f3dc9890f214c1860d02
Joe11S <joshaaron122@gmail.com>
2024-10-20 17:41:35 +0400

    Brute Force Attack
    The user must input the correct password. If the wrong password is entered, a loop will make the user input the password again.
    
    Only entering the correct password (in this case, 123456), can break the loop.

---
+++ Exercise 6/Brute Force Attack.py
@@ -0,0 +1,14 @@
+#Exercise 6 - Brute Force attack
+
+#user must input the password
+password = input("Enter your Password: ")
+
+#define the correct password
+correct_password = "123456"
+
+#check the password if it's correct
+while correct_password != password:
+    print("Wrong Password!")
+    password = input("Enter your Password: ")
+else:
+    print("Correct Password!")
