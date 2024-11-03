c0cf73e24ff0a6b1920941c38e4ca8214f2788f6
Joe11S <joshaaron122@gmail.com>
2024-11-03 18:02:44 +0400

    Some Counting
    A series of Loops in Python, printed.

---
+++ Exercise 7/Some Counting.py
@@ -0,0 +1,42 @@
+#Exercise 7 - Some Counting
+
+#to count from 0 to 50 in increment 1
+
+a = 0
+
+while a <=50:
+    print(a)
+    a = a + 1
+
+#to count from 50 to 0 decrement 1
+
+b = 50
+
+while b >=0:
+    print(b)
+    b = b - 1
+
+#to count from 30 to 50 increment 1
+
+c = 30
+
+while c <=50:
+    print(c)
+    c = c + 1
+
+#to count from 50 to 10 decrement 2
+
+d = 50
+
+while d >=10:
+    print(d)
+    d = d - 2
+
+#to count from 100 to 200 increment 5
+
+e = 100
+
+while e <=200:
+    print(e)
+    e = e + 5
+
