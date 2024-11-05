51a72993352de863e9eece501552606e2c4dfa77
Joe11S <joshaaron122@gmail.com>
2024-11-05 13:48:53 +0400

    Exercise 3 - Biography

---
+++ Exercise 3/biography advanced.py
@@ -0,0 +1,15 @@
+#Exercise 3 - Biography 
+
+#Take user input of the names
+name = input("Enter name: ")
+hometown = input("Enter hometown: ")
+age = int(input("Enter age: "))
+
+#Define the dictionary of the items in a biography.
+biography = {
+    "Name" : name,
+    "Hometown" : hometown,
+    "Age": age
+}
+
+print(biography)
---
+++ Exercise 3/biography basic.py
@@ -0,0 +1,10 @@
+#Exercise 3 - Biography 
+
+#Define the dictionary of the items in a biography.
+biography = {
+    "Name" : "Josh Aaron Alipin Boc",
+    "Hometown" : "Quezon City, Philippines",
+    "Age": "19"
+}
+
+print(biography)
