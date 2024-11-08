c24b860c9b1c3e95195ef55ee4f0ad764b8d3d84
Joe11S <joshaaron122@gmail.com>
2024-11-08 13:53:47 +0400

    Exercise 8 - Simple Search

--- Exercise 8/Simple Search.py
+++ Exercise 8/Simple Search.py
@@ -1,4 +1,12 @@
+#list down all the names.
 names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
+#ask for user input
 search_input = input("Search for your name: ")
+
+#search for list and find the name in it
+if search_input in names:
+    print(search_input, "is in the list of names.")
+else:
+    print(search_input, "not found.")
 
