675425e8d433989c636b8897903c0f052b239ece
Joe11S <joshaaron122@gmail.com>
2024-11-08 20:47:32 +0400

    Exercise 7 - Some Counting

--- Exercise 7/Some Counting.py
+++ Exercise 7/Some Counting.py
@@ -2,41 +2,30 @@
 
 #to count from 0 to 50 in increment 1
 
-a = 0
-
-while a <=50:
+print("From 0 to 50 increment 1")
+for a in range(51):
     print(a)
-    a = a + 1
 
 #to count from 50 to 0 decrement 1
 
-b = 50
-
-while b >=0:
+print("From 50 to 0 decrement 1")
+for b in range(50, -1, -1):
     print(b)
-    b = b - 1
 
 #to count from 30 to 50 increment 1
 
-c = 30
-
-while c <=50:
+print("From 30 to 50 increment 1")
+for c in range (30, 51):
     print(c)
-    c = c + 1
 
 #to count from 50 to 10 decrement 2
 
-d = 50
-
-while d >=10:
+print("From 50 to 10 decrement 2")
+for d in range(50, 8, -2):
     print(d)
-    d = d - 2
 
 #to count from 100 to 200 increment 5
 
-e = 100
-
-while e <=200:
+print("From 100 to 200 increment 5")
+for e in range(100, 205, 5):
     print(e)
-    e = e + 5
-
