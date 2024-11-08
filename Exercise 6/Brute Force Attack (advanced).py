45178c01b02f13cb13a4c6f8912a825111daf489
Joe11S <joshaaron122@gmail.com>
2024-11-08 14:34:52 +0400

    Exercise 6 - Brute Force Attack

--- Exercise 6/Brute Force Attack (advanced).py
+++ Exercise 6/Brute Force Attack (advanced).py
@@ -1,29 +1,33 @@
 #Exercise 6 - Brute Force attack
 
-#user must input the password
-password = input("Enter your Password to close the program: ")
-
 #define the correct password
 correct_password = "123456"
 
 #define attempts
-attempts = 4
+attempts = 5
 
 #check the password if it's correct
-while correct_password != password:
+while attempts > 0:
 
-    #break this loop if there are no attempts left.
-    if attempts == 0:
-        print("Authorities have been alerted.")
-        break
-
-    #if there are more attempts, repeat this loop until the above code breaks the loop.
-    print("Wrong Password!", str(attempts), "attempts left." )
+    #if there are more attempts, repeat this loop until the attempts reach 0.
     password = input("Enter your Password: ")
-    
-    #as long as least one attempt is left, the loop will decrement one attempt.
-    attempts = attempts - 1
 
-#if the user enters the correct password
+    #if the correct password is entered, exit the loop.
+    if password == correct_password:
+        print("Correct Password!")
+        break
+    
+    #if the wrong password is entered, keep decrementing attempts.
+    else:
+        attempts = attempts - 1
+        if attempts > 0:
+            
+            #extra pluralization block
+            if attempts > 1:
+                print("Wrong Password!", str(attempts), "attempts left." )
+            else:
+                print("Wrong Password!", str(attempts), "attempt left." )
+
+#alert authorities once attemps reach 0.
 else:
-    print("Correct Password!")
+    print("Authorities have been alerted.")
