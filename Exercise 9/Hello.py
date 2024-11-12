474f4c5b2a030c1a18377f41f725f6947b3bdb63
Joe11S <joshaaron122@gmail.com>
2024-11-12 13:25:26 +0400

    Exercise 9 - Hello

--- Exercise 9/Hello.py
+++ Exercise 9/Hello.py
@@ -1,2 +1,16 @@
-def main():
-    
+#Prints the hello function
+def Hello(name):
+    print("Hello,", name)
+
+#Calls the hello function
+def Main():
+    Hello(name)
+
+#Take User input
+name = input("Enter Name: ") 
+
+#Check condition
+if name == "Josh Aaron":
+    Main()
+else:
+    print("Unregistered Name.")
