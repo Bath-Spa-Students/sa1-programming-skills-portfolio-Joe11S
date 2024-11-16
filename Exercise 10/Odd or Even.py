1b3842d280c898c7b2ba1181b9554bd5154c00e6
Joe11S <joshaaron122@gmail.com>
2024-11-16 15:19:23 +0400

    Exercise 10 - Odd or Even

--- Exercise 10/Odd or Even.py
+++ Exercise 10/Odd or Even.py
@@ -1,13 +1,12 @@
 #Exercise 10 - Odd or Even
 
+#odd or even test block
 def odd_or_even_test(number):
     if number % 2 == 0:
         print("The number is even")
     else:
         print("The number is odd")
 
-def main():
-    odd_or_even_test(number)
-
-number = int(input("Enter your Number"))
-main()
+#take user input
+number = int(input("Enter your Number: "))
+odd_or_even_test(number)
